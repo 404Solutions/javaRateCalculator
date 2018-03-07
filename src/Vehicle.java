1999caeadb2e1d9c81abbe72521daa53ec247533
MeghanSnow <meghanashley313@gmail.com>
2018-03-07 15:42:40 -0330

    initial Vehicle Class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,55 @@
 public class Vehicle {
+    private int vehicleID;
+    private String make;
+    private int year;
+    private String model;
+    private String plateNumber;
+
+    public Vehicle(int vehicleID, String make, int year, String model, String plateNumber) {
+        this.vehicleID = vehicleID;
+        this.make = make;
+        this.year = year;
+        this.model = model;
+        this.plateNumber = plateNumber;
+    }
+
+    public int getVehicleID() {
+        return vehicleID;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setVehicleID(int vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
 }
