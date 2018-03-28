b96fad046b3a119b42a9b587ad84306b139f1039
rob <robseabright@gmail.com>
2018-03-28 16:18:45 -0230

    Updated SecondaryDriver to use the Date type.

--- src/Insurance/SecondaryDriver.java
+++ src/Insurance/SecondaryDriver.java
@@ -1,10 +1,12 @@
 package Insurance;
 
+import java.util.Date;
+
 public class SecondaryDriver extends Driver {
 
-    public SecondaryDriver(int userId, String firstName, String lastName, String dateOfBirth, String address, String city,
+    public SecondaryDriver(int userId, String firstName, String lastName, Date dateOfBirth, String address, String city,
                            String province, String postalCode, String phoneNumber, String email, String gender,
-                           String driversLicenceNumber, int licenceYearIssued, int locationCode) {
+                           String driversLicenceNumber, Date licenceYearIssued, int locationCode) {
 
         super (userId, firstName, lastName, dateOfBirth, address, city, province, postalCode, phoneNumber,
                 email, gender, driversLicenceNumber, licenceYearIssued, locationCode);
